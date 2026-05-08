81ac9b151d884f276f538c5035e625608ca37648
werbel7777 <dawidgorkowski1996@gmail.com>
2026-05-08 18:49:41 +0200

    trending movies details works

--- src/components/trendingMovie.tsx
+++ src/components/trendingMovie.tsx
@@ -1,14 +1,22 @@
+import { Link } from "react-router-dom";
+
 export const TrendingMovie = ({
   title,
   path,
+  id,
 }: {
   title: string;
   path?: string;
+  id: number;
 }) => {
   return (
-    <div className="shrink-0 w-48 bg-white/20 hover:bg-white/60 transition rounded p-3 text-white shadow-md">
-      <img src={`https://image.tmdb.org/t/p/w500${path}`} alt={title} />
-      <p className="drop-shadow-[0_2px_8px_black]">{title}</p>
-    </div>
+    <Link to={`/movie/${id}`}>
+      <div className="shrink-0 w-48 h-80 bg-white/20 hover:bg-white/60 transition rounded p-3 text-white shadow-md">
+        {path && (
+          <img src={`https://image.tmdb.org/t/p/w500${path}`} alt={title} />
+        )}
+        <p className="drop-shadow-[0_2px_8px_black]">{title}</p>
+      </div>
+    </Link>
   );
 };
