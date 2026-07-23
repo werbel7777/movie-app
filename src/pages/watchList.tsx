f73ed7b7cd078fdcaba91e80a90c36558d2bc846
werbel7777 <dawidgorkowski1996@gmail.com>
2026-07-23 16:41:48 +0200

    watchList done

---
+++ src/pages/watchList.tsx
@@ -0,0 +1,39 @@
+import type { RootState } from "../store/store";
+import { useSelector } from "react-redux";
+import { DisplayMovies } from "../components/displayMovies";
+import { useState, useEffect } from "react";
+import type { Movie } from "../types/types";
+import service from "../services/movieServices";
+import { Link } from "react-router-dom";
+import { Logo } from "../components/logo";
+
+export const WatchList = () => {
+  const watchList = useSelector(
+    (state: RootState) => state.watchList.watchList,
+  );
+
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const requests = watchList.map((id) => service.getDetails(String(id)));
+
+    Promise.all(requests).then((responses) => {
+      const watchListMovies = responses.map((response) => response.data);
+      setMovies(watchListMovies);
+    });
+  }, [watchList]);
+
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-blue-900 to-blue-500 p-10 text-white">
+      <Link to="/" className="relative z-50 mb-8 block w-fit cursor-pointer">
+        <Logo />
+      </Link>
+
+      <h1 className="text-3xl font-bold drop-shadow-[0_2px_8px_black]">
+        WatchList movies
+      </h1>
+
+      <DisplayMovies results={movies}></DisplayMovies>
+    </div>
+  );
+};
